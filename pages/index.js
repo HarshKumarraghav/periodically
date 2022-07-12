44dd4b491c9d9e1728159d3719fac86e7e3a6dcb
Harsh Raghav <=>
2022-07-12 11:40:01 +0530

    Frontend project started

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Periodically</title>
+        <link rel="icon" href="/favicon192.png" />
+      </Head>
+      hello world
+    </div>
+  );
+};
+
+export default Home;
