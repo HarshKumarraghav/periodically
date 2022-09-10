130603739ad42fa8bd7d7e4c7a199d01641eedbd
Harsh Raghav <=>
2022-09-11 01:51:48 +0530

    custom 404 error page is done 💡

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import Head from "next/head";
 import error404 from "../assets/error404.png";
 const Custom404 = () => {
   return (
-    <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
+    <>
+      <Head>
+        <title>ERROR-404</title>
+        <link rel="icon" href="/periodically_favicon.png" />
+      </Head>
+      <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
         <div className="w-full md:w-1/2  h-full md:h-1/4  flex justify-center items-center">
-              <Image src={error404} objectFit="fill"/>
+          <Image src={error404} objectFit="fill" />
         </div>
-    
-    </div>
+      </div>
+    </>
   );
 };
 
--- pages/history.js
+++ pages/history.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import Header from '../components/Header'
-import Head from 'next/head'
+import React from "react";
+import Header from "../components/Header";
+import Image from "next/image";
+import Comingsoon from '../assets/comingsoon.png';
+import Head from "next/head";
 const history = () => {
   return (
- <>
-   <Head>
+    <>
+      <Head>
         <title>Periodically-History</title>
         <link rel="icon" href="/periodically_favicon.png" />
       </Head>
- <Header/>
-    <div className='w-screen h-screen flex justify-center items-center bg-primary text-6xl text-primary-color'>Coming soon.....</div>
- </>
-  )
-}
+      <Header />
+      <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
+        <div className="w-full md:w-1/2  h-full md:h-1/4  flex justify-center items-center">
+          <Image src={Comingsoon} objectFit="fill" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default history
+export default history;
