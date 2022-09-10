a5b98c55746edeaec5e2b19d458a1db7c0412b38
Harsh Raghav <=>
2022-09-11 01:57:30 +0530

    history coming soon page update 🔮

--- pages/history.js
+++ pages/history.js
@@ -10,9 +10,10 @@ const history = () => {
         <title>Periodically-History</title>
         <link rel="icon" href="/periodically_favicon.png" />
       </Head>
+     
+      <div className="w-screen h-screen bg-primary overflow-hidden">
       <Header />
-      <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
-        <div className="w-full md:w-1/2  h-full md:h-1/4  flex justify-center items-center">
+        <div className="w-full  h-full  flex justify-center items-center">
           <Image src={Comingsoon} objectFit="fill" />
         </div>
       </div>
