3eb2dedbc9abf2ed979585fa6f575846ac4c38a8
Harsh Raghav <=>
2022-09-11 01:42:47 +0530

    custom 404 error page is done 🚫

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import error404 from "../assets/error404.png";
+const Custom404 = () => {
+  return (
+    <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
+        <div className="w-full md:w-1/2  h-full md:h-1/2  flex justify-center items-center">
+              <Image src={error404} objectFit="fill"/>
+        </div>
+    
+    </div>
+  );
+};
+
+export default Custom404;
