e9e9acf64c0ac44f35f21b29b468c259078a6a5b
Harsh Raghav <=>
2022-09-11 01:44:19 +0530

    custom 404 error page is done 💡

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import error404 from "../assets/error404.png";
 const Custom404 = () => {
   return (
     <div className="w-screen h-screen bg-primary overflow-hidden flex justify-center items-center">
-        <div className="w-full md:w-1/2  h-full md:h-1/2  flex justify-center items-center">
+        <div className="w-full md:w-1/2  h-full md:h-1/4  flex justify-center items-center">
               <Image src={error404} objectFit="fill"/>
         </div>
     
